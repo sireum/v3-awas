06453b55f2b8391da1cb1e96d1ed36c5270f9b14
Hariharan Thiagarajan <thari@santoslab.org>
2018-03-02 12:48:09 -0600

    cleaning up readme and jar builder

--- buildSireumJar.sh
+++ buildSireumJar.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-
-../bin/sbt-launch.sh test assembly
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DIR
+$DIR/../bin/sbt-launch.sh test assembly
+echo 'Sireum.jar generated in '$( cd "$( dirname "$DIR" )"/bin/ && pwd)
