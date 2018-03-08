7359736f7d24d09a11d1a73491cf096bb370fbfc
Hariharan Thiagarajan <thari@santoslab.org>
2018-03-08 16:44:53 -0600

    Generating awas string directly

--- jvm/src/main/java/org/sireum/awas/AADLBridge/AadlHandler.java
+++ jvm/src/main/java/org/sireum/awas/AADLBridge/AadlHandler.java
@@ -2,6 +2,7 @@ package org.sireum.awas.AADLBridge;
 
 import org.sireum.aadl.skema.ast.Aadl;
 import org.sireum.awas.ast.Model;
+import org.sireum.awas.ast.PrettyPrinter;
 import org.sireum.awas.slang.Aadl2Awas;
 
 public class AadlHandler {
@@ -10,16 +11,13 @@ public class AadlHandler {
         return Aadl2Awas.apply(aadlModel);
     }
 
-    public static void generateWitness(String awasFile,
-                                       String queryFile,
-                                       String output) {
-        Aadl2Awas.generateVisualizer(awasFile, queryFile, output);
+    public static String buildAwasString(Aadl aadlModel) {
+        return PrettyPrinter.apply(Aadl2Awas.apply(aadlModel));
     }
 
     public static void generateWitness(String awasFile,
                                        String queryFile,
-                                       String output,
-                                       String sireumJarLoc) {
-        Aadl2Awas.generateVisualizer(awasFile, queryFile, output, sireumJarLoc);
+                                       String output) {
+        Aadl2Awas.generateVisualizer(awasFile, queryFile, output);
     }
 }
