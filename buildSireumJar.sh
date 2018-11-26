d3c479260e8703f7fe2ac5b63daa0de59fe974e7
Hariharan Thiagarajan <thari@santoslab.org>
2018-11-26 12:31:08 -0600

    doc init

--- buildSireumJar.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo $DIR
-$DIR/../bin/sbt-launch.sh test assembly
-echo 'Sireum.jar generated in '$( cd "$( dirname "$DIR" )"/bin/ && pwd)
